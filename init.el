6c83f7e5f46bbc99d4633b1222b5e999d77b6653
Mark Krutzler <mark.krutzler@icloud.com@>
2024-05-07 22:40:35 +0200

    initial commit

---
+++ init.el
@@ -0,0 +1,28 @@
+;; Personal Emacs Configuration - Last Edit: 2024-05-07
+
+(setq inhibit-startup-screen t)                    ; don't use the splashscreen
+
+
+(setopt x-underline-at-descent-line nil)           ; Prettier underlines
+
+;; Enable horizontal scrolling
+(setopt mouse-wheel-tilt-scroll t)
+(setopt mouse-wheel-flip-direction t)
+
+;; Display line numbers in programming mode
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(setopt display-line-numbers-width 3)           ; Set a minimum width
+
+
+;; Nice line wrapping when working with text
+(add-hook 'text-mode-hook 'visual-line-mode)
+
+
+(tool-bar-mode -1)                      ; All these tools are in the menu-bar anyway
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Mark Krutzler"
+      user-mail-address "mark.krutzler@icloud.com")
